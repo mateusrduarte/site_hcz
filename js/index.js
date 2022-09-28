a9b2a97b210442e15a562e71a4a1517964b3731e
Mateus Duarte <mateusrduarte@gmail.com>
2022-09-28 17:54:33 -0300

    Cabeçalho e menu lateral.
    Foram feitos ajustes no HTML do cabeçalho e foi criado o menu lateral de navegação do site.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Animação para aparecer o menu lateral
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
+})
